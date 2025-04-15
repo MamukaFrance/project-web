8c9f0e4c40f1cc2ef1a0f6c04eb5afe46c35a2f5
Mamuka Bagatelia <mamuka.bagatelia2025@campus-eni.fr>
2025-04-15 11:27:39 +0200

    allfiles

---
+++ script.js
@@ -0,0 +1,9 @@
+let nom = $('#nom').on("input", function(){
+   
+    $('#nom').css("color", "red")
+    console.log( $(this).lenght)
+    if (nom.lengt < 3) {
+        
+    }
+    console.log("test")
+})
